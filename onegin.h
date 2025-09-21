ffbc7cd660367bdef1752ad3fd16afa0aa86ea0e
katefaiz <fem.e@mail.ru>
2025-09-21 21:37:10 +0300

    Разделила на файлы

---
+++ onegin.h
@@ -0,0 +1,22 @@
+#ifndef ONEGIN_H
+#define ONEGIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+
+enum Errors {
+    NO_ERROR        = 0,
+    OPENFILE_ERROR  = 1,
+    MEMORY_ERROR    = 2
+};
+
+void failed_funk(Errors failed);
+Errors counting_strings(char ** buffer, int * line_cnt);
+Errors array_filling(char ** buffer, int line_cnt, char ***text);
+int swap (char **ptr1, char **ptr2);
+int compare(const void * x1, const void * x2);
+void buble_sort(void * ptr, size_t count_lines, size_t size, int ( * compare) (const void * x1, const void * x2));
+
+#endif
